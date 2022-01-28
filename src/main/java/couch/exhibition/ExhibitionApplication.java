a3adeab6100e895ec02440810aaf274727a1f3cb
Suziny <tnwls6779@gmail.com>
2022-01-28 15:24:22 +0900

    project create

---
+++ src/main/java/couch/exhibition/ExhibitionApplication.java
@@ -0,0 +1,13 @@
+package couch.exhibition;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExhibitionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExhibitionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/couch/exhibition/ExhibitionApplicationTests.java
@@ -0,0 +1,13 @@
+package couch.exhibition;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExhibitionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
