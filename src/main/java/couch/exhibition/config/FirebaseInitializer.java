dfc0d9ee1641245a50212e712cf20e04df6fc844
dhkimneer <kimds989@naver.com>
2022-02-09 00:57:55 +0900

    Build: firebase admin 버전 변경 및 FirebaseInitializer 코드 수정

--- src/main/java/couch/exhibition/config/FirebaseInitializer.java
+++ src/main/java/couch/exhibition/config/FirebaseInitializer.java
@@ -7,14 +7,19 @@ import com.google.firebase.auth.FirebaseAuth;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import java.io.FileInputStream;
 import java.io.IOException;
 
 @Configuration
 public class FirebaseInitializer {
+
     @Bean
     public FirebaseAuth firebaseAuth() throws IOException {
+        FileInputStream serviceAccount =
+                new FileInputStream("src/main/resources/serviceAccountKey.json");
+
         FirebaseOptions options = FirebaseOptions.builder()
-                .setCredentials(GoogleCredentials.getApplicationDefault())
+                .setCredentials(GoogleCredentials.fromStream(serviceAccount))
                 .build();
         FirebaseApp.initializeApp(options);
         return FirebaseAuth.getInstance(FirebaseApp.getInstance());
