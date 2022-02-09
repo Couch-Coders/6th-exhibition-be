aa7a3987d0bc3dcbd2b59c3be6952341bde458b5
dhkimneer <kimds989@naver.com>
2022-02-09 19:17:27 +0900

    Chore: memberName notnull 추가

--- src/main/java/couch/exhibition/entity/Member.java
+++ src/main/java/couch/exhibition/entity/Member.java
@@ -1,21 +1,27 @@
 package couch.exhibition.entity;
 
+import com.google.firebase.database.annotations.NotNull;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NonNull;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import javax.persistence.*;
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
 
 @Entity
 @Getter
-public class Member {
+public class Member implements UserDetails {
 
     @Id @GeneratedValue
     @Column(name = "member_id")
     private Long id;
 
-    @Column(length = 50, name = "member_name", nullable = false)
+    @Column(length = 50, name = "member_name")
+    @NotNull
     private String memberName;
 
     @Column(length = 50)
@@ -30,6 +36,45 @@ public class Member {
     @Builder
     public Member(String memberName, String nickname) {
         this.memberName = memberName;
+        this.nickname = nickname;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return null;
+    }
+
+    @Override
+    public String getPassword() {
+        return null;
+    }
+
+    @Override
+    public String getUsername() {
+        return null;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return false;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return false;
+    }
+
+    public void editNickname(String nickname) {
         this.nickname = nickname;
     }
 }
