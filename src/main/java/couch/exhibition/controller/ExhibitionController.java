23c8494dd9ab617ae0960083dd30b4cc4aaa25b7
dhkimneer <kimds989@naver.com>
2022-02-10 21:39:29 +0900

    Fix: json parsing 코드 수정

--- src/main/java/couch/exhibition/controller/ExhibitionController.java
+++ src/main/java/couch/exhibition/controller/ExhibitionController.java
@@ -1,10 +1,12 @@
 package couch.exhibition.controller;
 
-import couch.exhibition.dto.ExhibitionDto;
 import couch.exhibition.entity.Exhibition;
 import couch.exhibition.repository.ExhibitionRepository;
-import org.json.simple.JSONObject;
-import org.json.simple.parser.JSONParser;
+import lombok.extern.slf4j.Slf4j;
+import org.json.XML;
+import org.json.JSONObject;
+
+//import org.json.simple.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -18,6 +20,7 @@ import java.net.URL;
 import java.nio.charset.StandardCharsets;
 import java.time.LocalDate;
 
+@Slf4j
 @Controller
 public class ExhibitionController {
 
@@ -31,30 +34,28 @@ public class ExhibitionController {
 
     @PostMapping("/api")
     public String load_save(@RequestParam("seq") String seq){
-        String result;
+        String result = "";
 
         try {
-            String serviceKey = "cqhL2N3Az%2BqDsTnmP5D0sUfmO7xujUBqG5gPWPxF7Ivv6eaIzWZtNrCBlyboVKnzjNY6gQviShM6JiC0DzKiGQ%3D%3D";
+            String serviceKey = "yu7NdPRhBWZtgcD4syUEm4DB3Vp%2BEVw05S%2BpLQvDHwzaRVSNvtFn6i9kuBLVia0LULNEFp3LASuS%2B%2B3iL4yP%2BA%3D%3D";
             URL url = new URL("http://www.culture.go.kr/openapi/rest/publicperformancedisplays/d/?" +
                                     "serviceKey=" + serviceKey + "&seq=" + seq);
 
             BufferedReader bf;
             bf = new BufferedReader(new InputStreamReader(url.openStream(), StandardCharsets.UTF_8));
             result = bf.readLine();
+            JSONObject resultJsonObject = XML.toJSONObject(result);
 
-            JSONParser jsonParser = new JSONParser();
-            JSONObject jsonObject = (JSONObject)jsonParser.parse(result);
-
-            JSONObject response = (JSONObject)jsonObject.get("response");
+            JSONObject response = (JSONObject) resultJsonObject.get("response");
             JSONObject msgBody = (JSONObject)response.get("msgBody");
 
             JSONObject perforInfo = (JSONObject)msgBody.get("perforInfo");
 
-            Exhibition infoObj = new Exhibition((String) perforInfo.get("title"), (String) perforInfo.get("place"),(String) perforInfo.get("city"),
-                    (BigDecimal) perforInfo.get("latitude"), (BigDecimal) perforInfo.get("longitude"),
+            Exhibition infoObj = new Exhibition((String) perforInfo.get("title"), (String) perforInfo.get("place"),(String) perforInfo.get("placeAddr"),
+                    (BigDecimal) perforInfo.get("gpsX"), (BigDecimal) perforInfo.get("gpsY"),
                     (LocalDate) perforInfo.get("startDate"), (LocalDate) perforInfo.get("endDate"),
-                    (String) perforInfo.get("contactLink"), (String) perforInfo.get("ticketPrice"),
-                    (String) perforInfo.get("reservationLink"), (String) perforInfo.get("posterUrl"));
+                    (String) perforInfo.get("placeUrl"), (String) perforInfo.get("price"),
+                    (String) perforInfo.get("url"), (String) perforInfo.get("imgUrl"));
 
             exhibitionRepository.save(infoObj);
 
