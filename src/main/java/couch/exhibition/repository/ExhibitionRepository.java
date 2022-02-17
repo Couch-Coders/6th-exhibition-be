aef2a2805628ad047ad4c5bb5cb86d1116da8445
Suziny <tnwls6779@gmail.com>
2022-02-17 19:57:50 +0900

    ExhibitionRepository 에 ãsearch 기능 작성

--- src/main/java/couch/exhibition/repository/ExhibitionRepository.java
+++ src/main/java/couch/exhibition/repository/ExhibitionRepository.java
@@ -1,7 +1,9 @@
 package couch.exhibition.repository;
 
+import couch.exhibition.dto.ExhibitionDto;
 import couch.exhibition.entity.Exhibition;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -10,15 +12,24 @@ import java.util.Optional;
 @Repository
 public interface ExhibitionRepository extends JpaRepository<Exhibition, Long> {
 
-    List<Exhibition> findById(String id);
 
+
+    @Query(value = "select * from Exhibition where title like %?0%", nativeQuery=true)
     Optional<Exhibition> findByTitle(String title);
 
-    List<Exhibition> findByDate(Integer startDate, Integer endDate);
+    @Query(value = "select start_date, end_date from Exhibition where to_date(start_date::TEXT, 'YYYYMMDD') <= now() and now()<=to_date(end_date::TEXT, 'YYYYMMDD')",nativeQuery=true)
+    List<ExhibitionDto> findByProgress();
 
-    List<Exhibition> findByPlace(String place);
+    List<ExhibitionDto> findByPlace(String place);
 
-    List<Exhibition> findByArea(String area);
+    @Query(value = "select * from Exhibition where place_addr like %?0% ", nativeQuery=true)
+    List<ExhibitionDto> findByCity(String city); //시도별 검색 예) 서울시
+
+    @Query(value = "select * from Exhibition where place_addr like %?0%",nativeQuery=true)
+    List<ExhibitionDto> findByArea(String area); //지역구별 검색 예) 서초구
+
+    @Query(value = "select * from Exhibition where place_addr like %?0% and place_addr like %?1%", nativeQuery=true)
+    List<ExhibitionDto> findByCityAndArea(String city, String area); //시도별 검색 예) 서울시
 
 
 }
--- src/main/java/couch/exhibition/service/ExhibitionService.java
+++ src/main/java/couch/exhibition/service/ExhibitionService.java
@@ -1,5 +1,6 @@
 package couch.exhibition.service;
 
+import couch.exhibition.repository.ExhibitionRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -9,6 +10,11 @@ import org.springframework.transaction.annotation.Transactional;
 @Service
 public class ExhibitionService {
 
+    private final ExhibitionRepository exhibitionRepository;
+
+    public ExhibitionService(ExhibitionRepository exhibitionRepository) {
+        this.exhibitionRepository = exhibitionRepository;
+    }
 
 
 }
