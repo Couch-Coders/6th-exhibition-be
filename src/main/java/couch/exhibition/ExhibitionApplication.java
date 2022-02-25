714f2a7e981925dbf1e1c597db206f91b484ab38
dhkimneer <kimds989@naver.com>
2022-02-25 21:06:39 +0900

    Feat: 공공 데이터 자동 DB 연결 구현 중

--- src/main/java/couch/exhibition/ExhibitionApplication.java
+++ src/main/java/couch/exhibition/ExhibitionApplication.java
@@ -2,8 +2,10 @@ package couch.exhibition;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling // 추가 설정(관련 설치?)
 public class ExhibitionApplication {
 
 	public static void main(String[] args) {
