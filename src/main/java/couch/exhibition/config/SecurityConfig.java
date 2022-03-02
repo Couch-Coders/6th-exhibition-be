b9534bd405fac5b69ce35ad5061e0fb3a3c18396
Suziny <tnwls6779@gmail.com>
2022-03-03 03:52:14 +0900

    SecurityConfig에 .failureForwardUrl(/pages) 추가

--- src/main/java/couch/exhibition/config/SecurityConfig.java
+++ src/main/java/couch/exhibition/config/SecurityConfig.java
@@ -34,12 +34,12 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .and()
                 .authorizeRequests()
                 .antMatchers(HttpMethod.OPTIONS, "/**").permitAll()
-                .antMatchers( "/login").permitAll()
                 .antMatchers("/api/v3/**", "health", "/swagger-ui.html", "/swagger/**", "/swagger-resources/**", "/webjars/**", "/v2/api-docs", "/swagger-ui/**").permitAll()
                 .anyRequest().authenticated().and()
                 .addFilterBefore(new JwtFilter(userDetailsService, firebaseAuth),
                         UsernamePasswordAuthenticationFilter.class)
-                .formLogin();
+                .formLogin()
+                    .failureForwardUrl("/pages");
 
 
 
