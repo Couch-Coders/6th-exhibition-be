75bbf62fbc793e4adc3a5b8a5f23f144f209f706
dhkimneer <kimds989@naver.com>
2022-04-06 19:53:51 +0900

    Chore: DB update test

--- src/main/java/couch/exhibition/ExhibitionApplication.java
+++ src/main/java/couch/exhibition/ExhibitionApplication.java
@@ -2,13 +2,21 @@ package couch.exhibition;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.scheduling.TaskScheduler;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.concurrent.ConcurrentTaskScheduler;
 
 @SpringBootApplication
-@EnableScheduling // 추가 설정(관련 설치?)
+@EnableScheduling
 public class ExhibitionApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ExhibitionApplication.class, args);
+	}
+
+	@Bean
+	public TaskScheduler taskScheduler() {
+		return new ConcurrentTaskScheduler();
 	}
 }
