e1509df750ada90b2ea7db436eb8f3c59c38cdac
dhkimneer <kimds989@naver.com>
2022-06-23 02:51:09 +0900

    Refactor: Remove JsonException by using Stringbuilder and use transactional annotation

--- src/main/java/couch/exhibition/service/DBService/ExhibitionDBService.java
+++ src/main/java/couch/exhibition/service/DBService/ExhibitionDBService.java
@@ -1,83 +1,83 @@
-package couch.exhibition.service.DBService;
-
-import couch.exhibition.config.ServiceConfig.RealmUrl;
-import couch.exhibition.config.ServiceConfig.ServiceKey;
-import couch.exhibition.exception.DBException.DBCustomException;
-import lombok.extern.slf4j.Slf4j;
-import org.json.JSONArray;
-import org.json.JSONObject;
-import org.springframework.scheduling.annotation.Scheduled;
-import org.springframework.stereotype.Component;
-import org.springframework.transaction.annotation.Transactional;
-
-import java.net.MalformedURLException;
-import java.net.URL;
-import java.time.LocalDate;
-import java.time.format.DateTimeFormatter;
-import java.util.ArrayList;
-import java.util.List;
-
-@Slf4j
-@Component
-public class ExhibitionDBService {
-
-    private final ServiceKey SERVICE_KEY;
-    private final RealmUrl REALM_URL;
-    private final ExhibitionGetDataService exhibitionGetDataService;
-    private final ExhibitionGetMsgBodyService exhibitionGetMsgBodyService;
-
-    public ExhibitionDBService(ServiceKey SERVICE_KEY, RealmUrl REALM_URL,
-                               ExhibitionGetDataService exhibitionGetDataService, ExhibitionGetMsgBodyService exhibitionGetMsgBodyService) {
-        this.SERVICE_KEY = SERVICE_KEY;
-        this.REALM_URL = REALM_URL;
-        this.exhibitionGetDataService = exhibitionGetDataService;
-        this.exhibitionGetMsgBodyService = exhibitionGetMsgBodyService;
-    }
-
-    /*
-    1. java.net.MalformedURLException (extends IOException) -> change into RuntimeException
-     */
-    @Scheduled(cron = "${regular.cron}")
-    @Transactional
-    public void load_save() {
-        String serviceKey = SERVICE_KEY.getServiceKey();
-        String realmUrl = REALM_URL.getRealmUrl();
-
-        int seqtotalCount = 0;
-        int pageNo = 1;
-        while (true){
-            URL url;
-            try {
-                url = new URL(realmUrl +
-                        "serviceKey=" + serviceKey +
-                        "&to=" + LocalDate.now().format(DateTimeFormatter.ofPattern("yyyyMMdd")) +
-                        "&realmCode=D000" +
-                        "&cPage=" + pageNo
-                );
-            } catch (MalformedURLException e) {
-                log.warn("malformed URL", e);
-                throw new DBCustomException("malformed URL");
-            }
-
-            JSONObject msgBody = exhibitionGetMsgBodyService.getMsgBody(url);
-
-            JSONArray perforList = (JSONArray) msgBody.get("perforList");
-
-            int totalCount = (int) msgBody.get("totalCount");
-
-            List<Integer> seqList = new ArrayList<>();
-            for (int index = 0; index < perforList.length(); index++) {
-                Integer sequence = (Integer) perforList.getJSONObject(index).get("seq");
-                seqList.add(sequence);
-            }
-
-            exhibitionGetDataService.getDBData(seqList);
-
-            seqtotalCount += seqList.size();
-
-            if(seqtotalCount == totalCount) break;
-
-            pageNo++;
-        }
-    }
-}
+//package couch.exhibition.service.DBService;
+//
+//import couch.exhibition.config.ServiceConfig.RealmUrl;
+//import couch.exhibition.config.ServiceConfig.ServiceKey;
+//import couch.exhibition.exception.DBException.DBCustomException;
+//import lombok.extern.slf4j.Slf4j;
+//import org.json.JSONArray;
+//import org.json.JSONObject;
+//import org.springframework.scheduling.annotation.Scheduled;
+//import org.springframework.stereotype.Component;
+//import org.springframework.transaction.annotation.Transactional;
+//
+//import java.net.MalformedURLException;
+//import java.net.URL;
+//import java.time.LocalDate;
+//import java.time.format.DateTimeFormatter;
+//import java.util.ArrayList;
+//import java.util.List;
+//
+//@Slf4j
+//@Component
+//public class ExhibitionDBService {
+//
+//    private final ServiceKey SERVICE_KEY;
+//    private final RealmUrl REALM_URL;
+//    private final ExhibitionGetDataService exhibitionGetDataService;
+//    private final ExhibitionGetMsgBodyService exhibitionGetMsgBodyService;
+//
+//    public ExhibitionDBService(ServiceKey SERVICE_KEY, RealmUrl REALM_URL,
+//                               ExhibitionGetDataService exhibitionGetDataService, ExhibitionGetMsgBodyService exhibitionGetMsgBodyService) {
+//        this.SERVICE_KEY = SERVICE_KEY;
+//        this.REALM_URL = REALM_URL;
+//        this.exhibitionGetDataService = exhibitionGetDataService;
+//        this.exhibitionGetMsgBodyService = exhibitionGetMsgBodyService;
+//    }
+//
+//    /*
+//    1. java.net.MalformedURLException (extends IOException) -> change into RuntimeException
+//     */
+//    @Scheduled(cron = "${regular.cron}")
+//    @Transactional
+//    public void load_save() {
+//        String serviceKey = SERVICE_KEY.getServiceKey();
+//        String realmUrl = REALM_URL.getRealmUrl();
+//
+//        int seqtotalCount = 0;
+//        int pageNo = 1;
+//        while (true){
+//            URL url;
+//            try {
+//                url = new URL(realmUrl +
+//                        "serviceKey=" + serviceKey +
+//                        "&to=" + LocalDate.now().format(DateTimeFormatter.ofPattern("yyyyMMdd")) +
+//                        "&realmCode=D000" +
+//                        "&cPage=" + pageNo
+//                );
+//            } catch (MalformedURLException e) {
+//                log.warn("malformed URL", e);
+//                throw new DBCustomException("malformed URL");
+//            }
+//
+//            JSONObject msgBody = exhibitionGetMsgBodyService.getMsgBody(url);
+//
+//            JSONArray perforList = (JSONArray) msgBody.get("perforList");
+//
+//            int totalCount = (int) msgBody.get("totalCount");
+//
+//            List<Integer> seqList = new ArrayList<>();
+//            for (int index = 0; index < perforList.length(); index++) {
+//                Integer sequence = (Integer) perforList.getJSONObject(index).get("seq");
+//                seqList.add(sequence);
+//            }
+//
+//            exhibitionGetDataService.getDBData(seqList);
+//
+//            seqtotalCount += seqList.size();
+//
+//            if(seqtotalCount == totalCount) break;
+//
+//            pageNo++;
+//        }
+//    }
+//}
--- src/main/java/couch/exhibition/service/DBService/ExhibitionGetMsgBodyService.java
+++ src/main/java/couch/exhibition/service/DBService/ExhibitionGetMsgBodyService.java
@@ -1,51 +1,51 @@
-package couch.exhibition.service.DBService;
-
-import couch.exhibition.exception.DBException.DBCustomException;
-import lombok.extern.slf4j.Slf4j;
-import org.json.JSONException;
-import org.json.JSONObject;
-import org.json.XML;
-import org.springframework.stereotype.Component;
-import org.springframework.transaction.annotation.Propagation;
-import org.springframework.transaction.annotation.Transactional;
-
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.net.URL;
-import java.nio.charset.StandardCharsets;
-
-@Slf4j
-@Component
-public class ExhibitionGetMsgBodyService {
-
-    /*
-    1. url.openStream() -> java.io.IOException -> change into RuntimeException
-    2. bf.readLine() -> IOException -> change into RuntimeException
-    3. org.json.JSONException (extends RuntimeException) : Unclosed tag
-     */
-    @Transactional(propagation = Propagation.REQUIRES_NEW)
-    public JSONObject getMsgBody(URL url) {
-        BufferedReader bf;
-        try {
-            bf = new BufferedReader(new InputStreamReader(url.openStream(), StandardCharsets.UTF_8));
-        } catch (IOException e) {
-            log.warn("URL IOException", e);
-            throw new DBCustomException("URL IOException");
-        }
-
-        String result = null;
-        try {
-            result = bf.readLine();
-        } catch (IOException e) {
-            log.warn("bf IOException", e);
-            throw new DBCustomException("bf IOException");
-        } catch (JSONException e) {
-            log.warn("Unclosed tag", e);
-        }
-
-        JSONObject resultJsonObject = XML.toJSONObject(result);
-        JSONObject response = (JSONObject) resultJsonObject.get("response");
-        return (JSONObject) response.get("msgBody");
-    }
-}
+//package couch.exhibition.service.DBService;
+//
+//import couch.exhibition.exception.DBException.DBCustomException;
+//import lombok.extern.slf4j.Slf4j;
+//import org.json.JSONException;
+//import org.json.JSONObject;
+//import org.json.XML;
+//import org.springframework.stereotype.Component;
+//import org.springframework.transaction.annotation.Propagation;
+//import org.springframework.transaction.annotation.Transactional;
+//
+//import java.io.BufferedReader;
+//import java.io.IOException;
+//import java.io.InputStreamReader;
+//import java.net.URL;
+//import java.nio.charset.StandardCharsets;
+//
+//@Slf4j
+//@Component
+//public class ExhibitionGetMsgBodyService {
+//
+//    /*
+//    1. url.openStream() -> java.io.IOException -> change into RuntimeException
+//    2. bf.readLine() -> IOException -> change into RuntimeException
+//    3. org.json.JSONException (extends RuntimeException) : Unclosed tag
+//     */
+//    @Transactional(propagation = Propagation.REQUIRES_NEW)
+//    public JSONObject getMsgBody(URL url) {
+//        BufferedReader bf;
+//        try {
+//            bf = new BufferedReader(new InputStreamReader(url.openStream(), StandardCharsets.UTF_8));
+//        } catch (IOException e) {
+//            log.warn("URL IOException", e);
+//            throw new DBCustomException("URL IOException");
+//        }
+//
+//        String result = null;
+//        try {
+//            result = bf.readLine();
+//        } catch (IOException e) {
+//            log.warn("bf IOException", e);
+//            throw new DBCustomException("bf IOException");
+//        } catch (JSONException e) {
+//            log.warn("Unclosed tag", e);
+//        }
+//
+//        JSONObject resultJsonObject = XML.toJSONObject(result);
+//        JSONObject response = (JSONObject) resultJsonObject.get("response");
+//        return (JSONObject) response.get("msgBody");
+//    }
+//}
